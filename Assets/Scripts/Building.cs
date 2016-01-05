b26be2356df201814ee9ccae3f6c09df1dda3b35
Kennety Andersson <kennetyandersson@gmail.com>
2016-01-05 13:52:31 -0300

    Criada a cena(blank) da TitleSceren.unity e adicionado o Scripts/Building.cs com as informações de cada construção

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	public string name;
+	public int cost;
+	public int citizenPerMonth;
+	public GameObject description;
+	public float available;
+
+	private float spaceToOccupy;
+	
+
+	void Start () {
+	}
+
+	void Update () {
+	}
+
+	void Build(){
+	}
+
+	void DestroyBuilding(){
+	}
+}
