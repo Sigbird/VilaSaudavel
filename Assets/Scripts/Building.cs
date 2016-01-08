3d5c552769b6b0e2943bdcf4111018e93dde90ad
Kennety Andersson <kennetyandersson@gmail.com>
2016-01-08 14:40:52 -0300

    Fazendo testes na interface

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -3,14 +3,45 @@ using System.Collections;
 
 public class Building : MonoBehaviour {
 
-	public string name;
-	public int cost;
-	public int citizenPerMonth;
-	public GameObject description;
-	public float available;
-
+	private string name;
+	private int cost;
+	private int citizenPerMonth;
+	private GameObject description;
+	private bool available;
 	private float spaceToOccupy;
 	
+	#region GETTERS and SETTERS
+	public string Name{
+		get{ return name;}
+		set { this.name = value;}
+	}
+
+	public int Cost{
+		get{ return cost;}
+		set{ this.cost = value;}
+	}
+
+	public int CitizesPerMonth{
+		get{ return citizenPerMonth;}
+		set{ this.citizenPerMonth = value;}
+	}
+
+	public GameObject Description{
+		get{ return description;}
+		set{ this.description = value;}
+	}
+
+	public bool Available{
+		get{ return available;}
+		set{ this.available = value;}
+	}
+
+	public float SpaceToOccupy{
+		get{ return spaceToOccupy;}
+		set{ this.spaceToOccupy = value;}
+	}
+	#endregion
+
 
 	void Start () {
 	}
