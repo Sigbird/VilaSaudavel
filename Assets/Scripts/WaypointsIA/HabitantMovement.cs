9138d95dad795f3e4710e8dba2da3bc9610aa9bb
Gustavo Mazzotti <gumazzotti@Yahoo.com.br>
2016-03-23 17:48:54 -0300

    Fix no tile de selecao das construcoes Implementado movimentaçao de habitantes via waypoints pelo mapa de maneira aleatória. Implementado prefab de habitante com 4 animações de movimentações já conectados com animator e sincronizados com velocity dele.

---
+++ Assets/Scripts/WaypointsIA/HabitantMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HabitantMovement : MonoBehaviour {
+	public float speed;
+	public GameObject next;
+	public Animator sprite;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		this.transform.position = Vector3.MoveTowards(transform.position, next.transform.position, Time.deltaTime * speed);
+//
+//		if (Vector3.Distance (this.transform.position, next.transform.position) <= )
+
+		FollowTargetWitouthRotation (this.next, Random.Range (0.1f, 0.3f), speed);
+		   
+		Debug.Log (GetComponent<Rigidbody> ().velocity);
+
+		sprite.SetFloat ("Speedx", rigidbody.velocity.x);
+		sprite.SetFloat ("Speedy", rigidbody.velocity.z);
+	}
+
+	void FollowTargetWitouthRotation(GameObject target, float distanceToStop, float speed)
+	{
+		var direction = Vector3.zero;
+		if (Vector3.Distance (transform.position, target.transform.position) > distanceToStop) {
+			direction = target.transform.position - transform.position;
+			rigidbody.AddRelativeForce (direction.normalized * speed, ForceMode.Force);
+		} else {
+			this.next = next.GetComponent<Waypoint> ().NextPoint ();
+			//FollowTargetWitouthRotation (this.next, Random.Range (0.1f, 0.3f), speed);
+		}
+	}
+
+}
---
+++ Assets/Scripts/WaypointsIA/Waypoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	public GameObject[] Waypoints;
+
+
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine ("senoid");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public GameObject NextPoint(){
+		return Waypoints [Random.Range (0, Waypoints.Length)];
+	}
+
+	IEnumerator senoid(){
+		transform.Translate (Vector3.forward * Time.deltaTime * 20);
+		yield return new WaitForSeconds (0.5f);
+		transform.Translate (Vector3.back* Time.deltaTime * 20);
+		yield return new WaitForSeconds (0.5f);
+		StartCoroutine ("senoid");
+	}
+}
